87738a9541eb996d69dae202dbc8ab297ccc2221
Yuko Shimizu <shimizu.y.af@gmail.com>
2016-03-25 14:44:56 +0900

    masterの変更を取り込む

---
+++ chat-tutorial/src/main/resources/static/js/javajo.js
@@ -0,0 +1,119 @@
+const EMPTY_NAME = 'ななし';
+const CHAT_MASTER_NAME = 'ぬし';
+
+/**
+ * 初期化処理
+ */
+var HelloStomp = function () {
+    this.connectButton = document.getElementById('connect');
+    this.disconnectButton = document.getElementById('disconnect');
+    this.sendButton = document.getElementById('send');
+    this.messageText = document.getElementById('message');
+
+    // イベントハンドラの登録
+    this.connectButton.addEventListener('click', this.connect.bind(this));
+    this.disconnectButton.addEventListener('click', this.disconnect.bind(this));
+    this.sendButton.addEventListener('click', this.sendName.bind(this));
+    this.messageText.addEventListener('input', this.setSendableStatus.bind(this));
+};
+
+/**
+ * エンドポイントへの接続処理
+ */
+HelloStomp.prototype.connect = function () {
+    var socket = new WebSocket('ws://' + location.host + '/endpoint'); // エンドポイントのURL
+    this.stompClient = Stomp.over(socket); // WebSocketを使ったStompクライアントを作成
+    this.stompClient.connect({}, this.onConnected.bind(this)); // エンドポイントに接続し、接続した際のコールバックを登録
+};
+
+/**
+ * エンドポイントへ接続したときの処理
+ */
+HelloStomp.prototype.onConnected = function (frame) {
+    console.log('Connected: ' + frame);
+    // 宛先が'/topic/messages'のメッセージを購読し、コールバック処理を登録
+    this.stompClient.subscribe('/topic/messages', this.onSubscribeGreeting.bind(this));
+    this.setConnected(true);
+};
+
+/**
+ * 受信したメッセージを画面に表示する処理
+ */
+HelloStomp.prototype.onSubscribeGreeting = function (message) {
+	
+	// 名前:messageを分解
+	s = message.body.split('：');
+	
+    var response = document.getElementById('response'); 
+    var p = document.createElement('p');
+    p.classList.add('talk-area');
+    response.appendChild(p);
+    
+    var name_span = document.createElement('span');
+    name_span.classList.add('talk-name');
+    name_span.appendChild(document.createTextNode(s[0]));
+    p.appendChild(name_span);
+    
+    var message_span = document.createElement('span');
+    message_span.classList.add('talk-content');
+
+    message_span.appendChild(document.createTextNode(s[1]));
+    p.appendChild(message_span);
+    
+    /** スクロールを一番下に **/
+    var scrollHeight = document.getElementById('response').scrollHeight;
+    document.getElementById('response').scrollTop = scrollHeight;
+};
+
+/**
+ * 宛先'/app/message'へのメッセージ送信処理
+ */
+HelloStomp.prototype.sendName = function () {
+    var name = document.getElementById('name').value;
+    if (!name) name = EMPTY_NAME;
+    // 宛先'/app/message'へメッセージを送信
+    this.stompClient.send('/app/message', {}, name + '：' + document.getElementById('message').value);  
+};
+
+/**
+* メッセージ入力に応じた送信ボタン表示の切り替え
+*/
+HelloStomp.prototype.setSendableStatus = function () {
+    var message = document.getElementById('message').value || '';
+    var connected = this.connectButton.disabled;
+    this.canSubmit(connected && message.length > 0);
+};
+
+/**
+ * 接続切断処理
+ */
+HelloStomp.prototype.disconnect = function () {
+    if (this.stompClient) {
+        var name = document.getElementById('name').value;
+        if (!name) name = EMPTY_NAME;
+        var disconnectMessage = CHAT_MASTER_NAME + '：' + name + 'さんが退室しました。';
+
+        this.stompClient.send('/app/message', {}, disconnectMessage);        
+        this.stompClient.disconnect();
+        this.stompClient = null;
+    }
+    this.setConnected(false);
+};
+
+/**
+ * 有効/無効切り替え
+ */
+HelloStomp.prototype.setConnected = function (connected) {
+    this.connectButton.disabled = connected;
+    this.disconnectButton.disabled = !connected;
+    this.setSendableStatus();
+};
+
+/**
+* 送信ボタン有効/無効切り替え
+*/
+HelloStomp.prototype.canSubmit = function (enabled) {
+    this.sendButton.disabled = !enabled;
+};
+
+new HelloStomp();
