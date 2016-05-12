542077d5a01159c3517c9abbaab767e5e3b27e65
Yuko Shimizu <shimizu.y.af@gmail.com>
2016-05-12 23:24:31 +0900

    ログイン機能
    セキュリティとテンプレートエンジンを使えるようpomに追加
    MvcConfigは演習用に空白にしておく
    WebSecurityConfigは難しいので完成済み

---
+++ chat-exercise/src/main/java/javajo/config/MvcConfig.java
@@ -0,0 +1,18 @@
+package javajo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * URLとViewのマッピングクラス.
+ */
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // TODO
+    }
+
+}
