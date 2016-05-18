62acd8622c18e91fa98dcd1009a2091987aa94e8
cyoco <ag052049@gmail.com>
2016-05-19 00:27:44 +0900

    指摘修正

--- chat-exercise/src/main/java/javajo/config/MvcConfig.java
+++ chat-exercise/src/main/java/javajo/config/MvcConfig.java
@@ -12,7 +12,6 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/login").setViewName("login");
         registry.addViewController("/welcome").setViewName("welcome");
         registry.addViewController("/chat").setViewName("chat");
     }
